cc70361469579a0621bbf26492e17ab1bf8e5e07
Chaitanya Abburu <chai@Mac.modem>
2026-04-14 22:01:23 +1000

    Remove predict_bucket from tools __init__

--- tools/__init__.py
+++ tools/__init__.py
@@ -1,4 +1,4 @@
-from .ml_tools import predict_regression, predict_bucket
+from .ml_tools import predict_regression
 from .rag_tools import search_similar_contracts
 
-__all__ = ["predict_regression", "predict_bucket", "search_similar_contracts"]
+__all__ = ["predict_regression", "search_similar_contracts"]
